93656930346b42e9c405c6b432513509e5a5a42d
phoebeliu123 <phoebeliu1--6@gmail.com>
2023-02-07 17:51:36 +0800

    ubuntu18.04 python3.6

--- scripts/docker/build-docker.sh
+++ scripts/docker/build-docker.sh
@@ -25,6 +25,6 @@
 # Build the CPU and GPU docker images.
 
 git archive --format=tar -o docker/gqcnn.tar --prefix=gqcnn/ master
-docker build --no-cache -t gqcnn/gpu -f docker/gpu/Dockerfile .
+# docker build --no-cache -t gqcnn/gpu -f docker/gpu/Dockerfile .
 docker build --no-cache -t gqcnn/cpu -f docker/cpu/Dockerfile .
 rm docker/gqcnn.tar
